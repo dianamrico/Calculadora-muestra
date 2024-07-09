2120597ac65867c50ae7aee480e0f6525a5c9fc7
dianamrico <dianita.rg@live.com>
2024-07-08 21:38:07 -0500

    se incluye gestion de errores

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const botones = document.querySelectorAll("#btn")
 const pantalla = document.querySelector("#pantalla")
+//const botones2 = document.querySelectorAll("button") usar esta linea para el proyecto propip en vez de la linea 1
 
 console.log(pantalla.textContent)
 botones.forEach (boton =>{
@@ -7,32 +8,36 @@ botones.forEach (boton =>{
     boton.addEventListener("click",()=>{
         const botonApretado = boton.textContent
         console.log(pantalla.textContent)
-        if (botonApretado == "AC"){
+        if (pantalla.textContent == "Error!"|| pantalla.textContent == "Infinity") {
             pantalla.textContent = "0"
-            return
-        }else if (botonApretado == "DEL"){
-            // agregar el caso de error y el caso infinity
-            if(pantalla.textContent.length == 1){ 
-                pantalla.textContent = "0" 
-                return
-            }else{
-                pantalla.textContent = pantalla.textContent.slice(0,-1)
-                return
-            }
-        }else if(botonApretado == "=") {
-            // agregar el caso de error y el caso infinity
-            try {
-                pantalla.textContent = eval(pantalla.textContent)
-            } catch (error) {
-                pantalla.textContent = "Error!"
-            }
         }else{
-            if(pantalla.textContent == "0"){
-                pantalla.textContent = botonApretado
+            if (botonApretado == "AC"){
+                pantalla.textContent = "0"
                 return
-            }else{ 
-                pantalla.textContent = pantalla.textContent + botonApretado
-                return
+            }else if (botonApretado == "DEL"){
+                
+                if(pantalla.textContent.length == 1){ 
+                    pantalla.textContent = "0" 
+                    return
+                }else{
+                    pantalla.textContent = pantalla.textContent.slice(0,-1)
+                    return
+                }
+            }else if(botonApretado == "=") {
+      
+                try {
+                    pantalla.textContent = eval(pantalla.textContent)
+                } catch (error) {
+                    pantalla.textContent = "Error!"
+                }
+            }else{
+                if(pantalla.textContent == "0"){
+                    pantalla.textContent = botonApretado
+                    return
+                }else{ 
+                    pantalla.textContent = pantalla.textContent + botonApretado
+                    return
+                }
             }
         }
         
